40546ed4e6d6a36048b51970ab6613542b3eaf3a
leilani-heather <leilaniheather@gmail.com>
2021-07-15 09:59:35 +1200

    Components created

--- server/db/seeds/pet_characteristics.js
+++ server/db/seeds/pet_characteristics.js
@@ -2,7 +2,7 @@ exports.seed = function (knex) {
   return knex('pet_characteristics').del()
     .then(function () {
       return knex('pet_characteristics').insert([
-        { id: 50000, actvity: 'high', sleephours: '12h daily', social_human: 'high', social_animal: 'medium-high', pet_id: 10000 }
+        { id: 50000, activity: 'high', sleephours: '12h daily', social_human: 'high', social_animal: 'medium-high', pet_id: 10000 }
       ])
     })
 }
--- server/db/seeds/pet_profile.js
+++ server/db/seeds/pet_profile.js
@@ -2,7 +2,7 @@ exports.seed = function (knex) {
   return knex('pet_profile').del()
     .then(function () {
       return knex('pet_profile').insert([
-        { id: 10000, name: 'good boy', date: new Date(2012, 1, 1), type: 'dog', breed: 'english mastiff', gender: 'female', user_id: 1 }
+        { id: 10000, name: 'good boy', dob: new Date(2012, 1, 1), type: 'dog', breed: 'english mastiff', gender: 'female', user_id: 1 }
       ])
     })
 }
