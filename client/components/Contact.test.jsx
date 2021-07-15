a637edad426a2cb2eb19413c25c435a6b1d05923
leilani-heather <leilaniheather@gmail.com>
2021-07-15 14:13:32 +1200

    Contact route and component tests in progress

---
+++ client/components/Contact.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import Contact from './Contact'
+
+jest.mock('../apis/index', () => {
+  return {
+    getNewPassword: jest.fn()
+  }
+})
+
+// test('render 1 input component', () => {
+//   expect.assertions(1)
+//   const { getByPlaceholderText } = render(<ResetPassword/>)
+//   expect(getByPlaceholderText('enter your email')).toBeInTheDocument()
+// })
