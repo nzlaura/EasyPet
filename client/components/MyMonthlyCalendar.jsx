a872074b638006aea7340f1b3aeb96974e66f151
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-16 16:47:23 +1200

    created seeds and migrations for calendar events

--- client/components/MyMonthlyCalendar.jsx
+++ client/components/MyMonthlyCalendar.jsx
@@ -15,43 +15,66 @@ export default function MyMonthlyCalendar () {
     startOfMonth(new Date())
   )
 
-  // const eventItems = [
-  //   { title: 'Call John', date: subHours(new Date(), 2) },
-  //   { title: 'Call John', date: subHours(new Date(), 1) },
-  //   { title: 'Meeting with Bob', date: new Date() }
-  // ]
+  const eventItems = [
+    { title: 'Call John', date: subHours(new Date(), 2) },
+    { title: 'Call John', date: subHours(new Date(), 1) },
+    { title: 'Meeting with Bob', date: new Date() }
+  ]
+  // const [eventItems, setEventItems] = useState([
+  //   {
+  //     title: '',
+  //     date: subHours(new Date())
+  //   }
+  // ])
 
-  const [eventItems, setEventItems] = useState([
-    {
-      title: '',
-      date: subHours(new Date())
-    }
-  ])
+  // onlick that takes in the date thats clicked, create event handler
+  // New state, newEventItem, setNewEventItem
+
+  // function handleChange (e) {
+  //   const { name, value } = e.target
+  //   setContactForm({
+  //     ...contactForm,
+  //     [name]: value
+  //   })
+  // }
+
+  // function handleClick (e) {
+  //   e.preventDefault()
+  //   sendContactFormMessage(contactForm)
+  //   setContactForm({
+  //     name: '',
+  //     email: '',
+  //     subject: '',
+  //     message: ''
+  //   })
+  //   // }
 
   return (
     <>
-      <MonthlyCalendar
-        currentMonth={currentMonth}
-        onCurrentMonthChange={date => setCurrentMonth(date)}
-      >
-        <MonthlyNav />
-        <MonthlyBody
-          events={eventItems}
+      <div>
+        <MonthlyCalendar
+          currentMonth={currentMonth}
+          onCurrentMonthChange={date => setCurrentMonth(date)}
         >
-          <MonthlyDay
-            renderDay={data =>
-              data.map((item, index) => (
-                <DefaultMonthlyEventItem
-                  key={index}
-                  title={item.title}
-                  // Format the date here to be in the format you prefer
-                  date={format(item.date, 'k:mm')}
-                />
-              ))
-            }
-          />
-        </MonthlyBody>
-      </MonthlyCalendar>
+          <MonthlyNav />
+          <MonthlyBody
+            events={eventItems}
+          >
+            <MonthlyDay
+              renderDay={data =>
+                data.map((item, index) => {
+                  return <DefaultMonthlyEventItem
+                    key={index}
+                    title={item.title}
+                    // Format the date here to be in the format you prefer
+                    date={format(item.date, 'k:mm')}
+                  />
+                })
+              }
+            />
+          </MonthlyBody>
+        </MonthlyCalendar>
+      </div>
     </>
   )
 }
