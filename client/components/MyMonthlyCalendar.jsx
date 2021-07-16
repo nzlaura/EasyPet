0903407fe7da4add7e5b6c84565c73ce8ef5380f
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-16 15:20:42 +1200

    Added react router dom

--- client/components/MyMonthlyCalendar.jsx
+++ client/components/MyMonthlyCalendar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { format, subHours, startOfMonth } from 'date-fns'
+import '@zach.codes/react-calendar/dist/calendar-tailwind.css'
+
 import {
   MonthlyBody,
   MonthlyDay,
@@ -7,41 +9,49 @@ import {
   MonthlyNav,
   DefaultMonthlyEventItem
 } from '@zach.codes/react-calendar'
-import '@zach.codes/react-calendar/dist/calendar-tailwind.css'
 
 export default function MyMonthlyCalendar () {
   const [currentMonth, setCurrentMonth] = useState(
     startOfMonth(new Date())
   )
 
+  // const eventItems = [
+  //   { title: 'Call John', date: subHours(new Date(), 2) },
+  //   { title: 'Call John', date: subHours(new Date(), 1) },
+  //   { title: 'Meeting with Bob', date: new Date() }
+  // ]
+
+  const [eventItems, setEventItems] = useState([
+    {
+      title: '',
+      date: subHours(new Date())
+    }
+  ])
+
   return (
-
-    <MonthlyCalendar
-      currentMonth={currentMonth}
-      onCurrentMonthChange={date => setCurrentMonth(date)}
-    >
-      <MonthlyNav />
-      <MonthlyBody
-        events={[
-          { title: 'Call John', date: subHours(new Date(), 2) },
-          { title: 'Call John', date: subHours(new Date(), 1) },
-          { title: 'Meeting with Bob', date: new Date() }
-        ]}
+    <>
+      <MonthlyCalendar
+        currentMonth={currentMonth}
+        onCurrentMonthChange={date => setCurrentMonth(date)}
       >
-        <MonthlyDay
-          renderDay={data =>
-            data.map((item, index) => (
-              <DefaultMonthlyEventItem
-                key={index}
-                title={item.title}
-                // Format the date here to be in the format you prefer
-                date={format(item.date, 'k:mm')}
-              />
-            ))
-          }
-        />
-      </MonthlyBody>
-    </MonthlyCalendar>
-
+        <MonthlyNav />
+        <MonthlyBody
+          events={eventItems}
+        >
+          <MonthlyDay
+            renderDay={data =>
+              data.map((item, index) => (
+                <DefaultMonthlyEventItem
+                  key={index}
+                  title={item.title}
+                  // Format the date here to be in the format you prefer
+                  date={format(item.date, 'k:mm')}
+                />
+              ))
+            }
+          />
+        </MonthlyBody>
+      </MonthlyCalendar>
+    </>
   )
 }
