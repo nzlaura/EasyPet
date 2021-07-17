7371c0141bf90084ea6979d4e24855a09c4bdccd
leilani-heather <leilaniheather@gmail.com>
2021-07-18 09:31:18 +1200

    More updates

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -45,15 +45,16 @@ function SignIn (props) {
     <>
       <div className='bg-fixed bg-cover' style={{ backgroundImage: `url(${signInImage})` }}>
         <div className='container h-screen'>
-          <form className='flex items-center grid grid-cols-1 grid-rows-5 w-3/12 h-64' id='signin-form'>
+          <form className='flex items-center grid grid-cols-1 grid-rows-6 w-3/12 h-64' id='signin-form'>
             <p className='text-5xl flex items-left font-bold mb-4 mt-52 ml-20 mb-20 text-black'>Login</p>
             <input className='rounded-md shadow-sm col-1 mt-52 ml-20 mb-10 h-12' type='text' id='username' name='username' placeholder='  Enter Username' onChange={(e) => setLoginUsername(e.target.value)}/>
             <input className='rounded-md shadow-sm col-1 mt-52 ml-20 h-12' type='password' id='passwordinput' name='passwordinput' placeholder='  Enter Password' onChange={(e) => setLoginPassword(e.target.value)}/>
             <label className='inline-flex items-center' htmlFor='showPassword'>
               <input className='form-checkbox col-1 mt-52 ml-20' type='checkbox' id='showPassword' onClick={showPassword}/>
-              <span className='ml-2 col-1 mt-52 text-black'>Show password</span>
+              <span className='ml-2 col-1 mt-52 text-sm text-black'>Show password</span>
             </label>
             <button className='bg-black hover:bg-gray-900 text-white font-bold rounded-md flex items-center justify-center col-1 h-12 mt-52 ml-20' type='submit' onClick={login}>Submit</button>
+            <p className="col-1 mt-72 ml-20 mb-10 h-12 text-white text-sm">Need an account? <a href="#/register"> Sign up</a></p>
           </form>
         </div>
       </div>
