9efe06c3294a31cf7c7b1d49c17038ba24726dfc
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-07-18 21:48:36 +1200

    last touch up style changes

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -59,10 +59,10 @@ function Register (props) {
       <div className='bg-scroll bg-cover' style={{ backgroundImage: `url(${signInImage})` }}>
         <div className='container h-screen'>
           <form className='flex items-center grid grid-cols-1 grid-rows-6 w-3/12 h-64' id='register-form'>
-            <p className='text-5xl flex items-left font-bold mb-4 mt-48 ml-20 mb-20 text-black'>Sign Up</p>
+            <p className='text-5xl flex items-left font-bold mb-4 mt-44 ml-20 mb-20 text-black'>Sign Up</p>
             <input className='rounded-md shadow-sm col-1 mt-52 ml-20 mb-10 h-12 w-60 p-4' type='text' id='username' name='username' placeholder='Create Username' onChange={(e) => setRegisterUsername(e.target.value)}/>
             <input className='rounded-md shadow-sm col-1 mt-56 ml-20 h-12 w-60 p-4' type='password' id='passwordinput' name='passwordinput' placeholder='Create Password' onChange={(e) => setRegisterPassword(e.target.value)}/>
-            <label className='col-1 mt-60 ml-20 text-white font-bold' for='showPassword'><input className='form-checkbox' type='checkbox' id='showPassword' onClick={showPassword}/> Show Password</label>
+            <label className='col-1 mt-60 ml-20 text-white font-bold' htmlFor='showPassword'><input className='form-checkbox' type='checkbox' id='showPassword' onClick={showPassword}/> Show Password</label>
             <button className='bg-black hover:bg-gray-900 text-white font-bold rounded-md flex items-center justify-center col-1 h-12 mt-64 w-40 ml-20' type='submit' onClick={register}>Submit</button>
             <p className="col-1 mt-80 ml-20 mb-10 h-12 text-white text-sm font-bold">Already have an account? <a href="#/login"> Sign in</a></p>
           </form>
