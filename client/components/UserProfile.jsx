3c6a54b6294af1a212b5bc9c5eb9ee206164e7da
adam-king-10 <adam.t.king10@gmail.com>
2021-07-18 15:00:12 +1200

    components setup

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -1,29 +1,26 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import intlTelInput from 'intl-tel-input';
+import AddressFinder from './AddressFinder'
 
 function UserProfile (props) {
-  //Create div and elements
-  //gather from state the logged in user
-  //map the users details into the props
-  //Create functions for onchange
-  //Create functions for onclick to update DB
-    const phoneInputField = document.selectElementById('phone');
-    const phoneInput = intlTelInput(phoneInputField, {
-      utilsScript:
-        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-    });
+  const {userName, updateUserName} = useState('')
+  const {email, updateEmail} = useState('')
+  const {phoneNumber, updatePhoneNumber} = useState('')
+  const {firstName, updateFirstName} = useState('')
+  const {lastName, updateLastName} = useState('')
+  const {dob, updateDob} = useState('')
 
-    const info = document.querySelector(".alert-info");
+  
 
-    function process(event) {
-    event.preventDefault();
-
-    const phoneNumber = phoneInput.getNumber();
-
-    info.style.display = "";
-    info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
+  
+  function showPassword () {
+    var x = document.getElementById('passwordinput')
+    if (x.type === 'password') {
+      x.type = 'text'
+    } else {
+      x.type = 'password'
     }
+  }
 
   return (
     <>
@@ -31,19 +28,21 @@ function UserProfile (props) {
       <p className='text-5xl items-left font-bold mb-4 mt-52 ml-20 mb-20 text-white'>User Profile</p>
       <p className='text-2xl items-left font-bold mb-4 mt-52 ml-20 mb-20 text-white'>EasyPet Account Details:</p>
       <form className='' id='profile-form'>
-        <label className=''>Username</label>
+        <label for='username' className=''>Username</label>
         <input className='rounded-md shadow-sm col-1 mt-52 ml-20 mb-10 h-12' type='text' id='username' name='username' placeholder='Enter Username' onChange={(e) => updateUserName(e.target.value)}/>
-        <label className=''>Email</label>
+        <label for='email' className=''>Email</label>
         <input className='rounded-md shadow-sm col-1 mt-52 ml-20 mb-10 h-12' type='text' id='email' name='email' placeholder='Enter Email Address' onChange={(e) => updateEmail(e.target.value)}/>      
-        <label className=''>Phone Number</label>
-        <input className='rounded-md shadow-sm col-1 mt-52 ml-20 mb-10 h-12' type='int' id='email' name='email' placeholder='Enter Email Address' onChange={(e) => updatePhoneNumber(e.target.value)}/>      
-        <div class="alert alert-info" style="display: none;"></div>   
+        <label for='phone' className=''>Phone Number</label>
+        <input className='rounded-md shadow-sm col-1 mt-52 ml-20 mb-10 h-12' type='int' id='phone' name='phone' placeholder='Enter Phone Number' onChange={(e) => updatePhoneNumber(e.target.value)}/>      
       </form>
-      <form id="login" onsubmit="process(event)">
-        <p>Enter your phone number:</p>
-        <input id="phone" type="tel" name="phone" />
-        <input type="submit" class="btn" value="Verify" />
-        </form>
+      <form className='resetpwd'>
+        <label for='passwordinput' className=''>Update Password</label>
+        <input className='rounded-md shadow-sm col-1 mt-52 ml-20 h-12' type='password' id='passwordinput' name='passwordinput' placeholder='Enter New Password' onChange={(e) => updatePassword(e.target.value)}/>
+        <label className='' htmlFor='showPassword'>
+        <input className='form-checkbox col-1 mt-52 ml-20' type='checkbox' id='showPassword' onClick={showPassword}/>
+        <span className='ml-2 col-1 mt-52 text-white'>Show password</span>
+        </label>
+      </form>
       <p className='text-2xl items-left font-bold mb-4 mt-52 ml-20 mb-20 text-white'>Personal Details:</p>
       <form className='' id='profile-form'>
         <label className=''>First Name</label>
@@ -53,6 +52,7 @@ function UserProfile (props) {
         <label className=''>Date of Birth</label>
         <input className='rounded-md shadow-sm col-1 mt-52 ml-20 mb-10 h-12' type='date' id='dob' name='dob' placeholder='Enter DOB' onChange={(e) => updateDob(e.target.value)}/>
       </form>
+      <AddressFinder/>
     </div>
     </>
   )
