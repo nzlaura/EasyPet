6057c63572c63f6af6a0d88ebbf6e4c6b1c0bafc
leilani-heather <leilaniheather@gmail.com>
2021-07-20 18:09:59 +1200

    removed colons from user profile

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -26,7 +26,7 @@ function UserProfile (props) {
       <div className='bg-scroll bg-cover' style={{ backgroundImage: `url(${profileImage})` }}>
         <div className='container h-screen inline'>
           <p className='text-5xl items-left font-bold mb-5 ml-20 text-black'>User Profile</p>
-          <p className='text-2xl items-left font-bold text-white ml-20'>Account Details:</p>
+          <p className='text-2xl items-left font-bold text-white ml-20'>Account Details</p>
           <form className='flex items-center grid grid-cols-1 grid-rows-6 w-3/12 h-64' id='profile-form'>
             <label className='ml-20' htmlFor='username'>Username</label>
             <input className='rounded-md shadow-sm col-1 h-12 ml-20 p-4' type='text' id='username' name='username' placeholder='Enter Username' onChange={(e) => handleChange(e.target.value)}/>
@@ -35,7 +35,7 @@ function UserProfile (props) {
             <label className='ml-20' htmlFor='phone'>Phone Number</label>
             <input className='rounded-md shadow-sm col-1 h-12 ml-20 p-4' type='int' id='phone' name='phone' placeholder='Enter Phone Number' onChange={(e) => handleChange(e.target.value)}/>
           </form>
-          <p className='text-2xl items-left font-bold text-white mt-12 ml-20'>Personal Details:</p>
+          <p className='text-2xl items-left font-bold text-white mt-12 ml-20'>Personal Details</p>
           <form className='flex items-center grid grid-cols-1 grid-rows-6 w-3/12 h-64 mb-2' id='profile-form'>
             <label className='ml-20'>First Name</label>
             <input className='rounded-md shadow-sm col-1 h-12 ml-20 p-4' type='text' id='firstName' name='firstName' placeholder='Enter First Name' onChange={(e) => handleChange(e.target.value)}/>
