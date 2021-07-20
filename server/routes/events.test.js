3a4f9ee080877fdc65519f7cc482cddc27c16502
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-21 10:38:43 +1200

    POST route tests for events route passing

--- server/routes/events.test.js
+++ server/routes/events.test.js
@@ -1,10 +1,8 @@
 const request = require('supertest')
 const server = require('../server')
 const { getAllEvents, addNewEvent } = require('../db/db')
-// const { restore } = require('nock/types')
 
 jest.mock('../db/db')
-jest.mock('')
 
 describe('GET api/v1/events', () => {
   test('event route', () => {
@@ -41,7 +39,29 @@ describe('GET api/v1/events', () => {
 describe('POST api/v1/events', () => {
   test('events are sent correctly', () => {
     addNewEvent.mockImplementation(() => {
-
+      return Promise.resolve([1])
     })
+    return request(server)
+      .post('/api/v1/events')
+      .expect(201)
+      .then(res => {
+        expect(res.status).toEqual(201)
+        expect(res.body).toBe(1)
+        return null
+      })
+  })
+  test('events do not send correctly', () => {
+    addNewEvent.mockImplementation(() => {
+      const err = new Error('Oops, something went wrong')
+      return Promise.reject(err)
+    })
+    return request(server)
+      .get('/api/v1/events')
+      .expect(500)
+      .then(res => {
+        expect(res.status).toEqual(500)
+        expect(res.text).toContain('Oops')
+        return null
+      })
   })
 })
