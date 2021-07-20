58db739b671e1cd3534f38aad1d04a37f890fcfb
leilani-heather <leilaniheather@gmail.com>
2021-07-20 16:36:00 +1200

    Formatting of home text fixed

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -34,7 +34,7 @@ function Home () {
           <img src={img1}/>
         </div>
         <div>
-          <p className='text-6xl flex text-left font-bold mt-36 mb-4 ml-30'>Never miss a vet appointment</p>
+          <p className='text-6xl flex text-left font-bold mt-36 mb-4 ml-36'>Never miss a vet appointment</p>
           <p className='text-3xl flex text-left mb-12 ml-36'>EasyPet assists pet owners in caring for and managing their pets</p>
           <button className='bg-black hover:bg-gray-900 text-white font-bold text-xl rounded-md flex items-center justify-center ml-36 h-20 w-60' type='submit'>
             <a href="#/faq">Learn More</a>
