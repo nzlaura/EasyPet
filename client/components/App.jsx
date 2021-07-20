fdb00aef175b34e78ae5cdc4a5fb9f7d6c8b671a
adam-king-10 <adam.t.king10@gmail.com>
2021-07-21 09:03:11 +1200

    created addPet component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,6 +7,7 @@ import Register from './Register'
 import SignIn from './SignIn'
 import UserProfile from './UserProfile'
 import PetProfile from './PetProfile'
+import AddPet from './AddPet'
 import MyMonthlyCalendar from './MyMonthlyCalendar'
 import Header from './Header'
 import Footer from './Footer'
@@ -35,6 +36,7 @@ function App (props) {
       <Route exact path = '/login' component={SignIn} />
       <Route exact path = '/user' component={UserProfile} />
       <Route exact path = '/user/pets' component={PetProfile} />
+      <Route exact path = '/user/pets/addpet' component={AddPet} />
       <Route exact path = '/faq' component={FaqList} />
       <Route exact path="/search/:searchString" component={SearchFAQ} />
       <Route path = '/about' component={About} />
