96c229c8e69fd408cc2901ef54591c101841a97d
leilani-heather <leilaniheather@gmail.com>
2021-07-20 16:58:53 +1200

    added terms of service and privacy policy

---
+++ client/components/TermsOfUse.jsx
@@ -0,0 +1,96 @@
+// My Rover Limited - General Terms and conditions
+// These general terms and conditions apply to all users (‘You’) of the My Rover Limited (‘we’, ‘us’) website and mobile application (‘App’ or ‘Apps’) (together the ‘Platforms’). By viewing and using the Platforms you are deemed to agree to these terms and conditions without qualification. If you do not agree to be bound by these terms and conditions, you must stop accessing and using the Platforms.
+
+// The Platforms are operated by My Rover Limited from New Zealand. The information on the Platforms may not be appropriate or available for use in other jurisdictions. If you choose to access the Platforms from a jurisdiction other than New Zealand, you do so on your own initiative and you are responsible for compliance with any applicable laws of that jurisdiction.
+
+// Privacy policy and personal information:
+// Information collected through the Platforms may include your personal information, and you agree to the terms of our privacy policy [www.myrover.co/privacy-statement].
+
+// By registering on the Platforms, you consent to receiving marketing, promotional and other material by way of electronic messages from us.
+
+// Registration and accounts:
+// When you complete a registration in relation to the Platforms it will be either as:
+
+// an animal owner;
+// a verified veterinarian/animal clinician; or
+// another approved user.
+// A veterinarian/animal clinician will not be able to use the Platforms until their registration has been verified by the New Zealand Veterinary Association.
+// Where you complete a registration in relation to the Platforms:
+
+// Password security: You will ensure that all usernames and passwords required to access the Platforms are kept secure and confidential and you will notify us immediately of any unauthorised use of your password or any other breach of security.
+// Account transfer: You may not transfer your account to another user or maintain more than one account with us without our consent.
+// Registration data: You confirm that the data you provided to us on registering as a registered user was at the time you provided it current, complete and accurate, and you agree to maintain and update the data as required to keep it so.
+// Reliance upon password: You release us from and renounce any claim in respect of or arising from any reasonable reliance by us upon any use of your password, including us disclosing information relating to your registration and/or affairs with us. You agree to indemnify and hold us, our officers and employees harmless from any claims, actions, costs (including legal costs), or losses by us or any third party due to or arising out of reasonable reliance by us or any third party upon any acts carried out, requests made or information provided through the use of your password.
+// Suspension and termination: We may, at our sole discretion, suspend or terminate your account and limit your access to the Platforms.
+// Copyright, Trademarks and Limited Reproduction Notices:
+// The design and contents of the Platforms are the copyright of My Rover Limited or suppliers to us. No part of the Platforms or information contained within may be distributed or copied for any commercial purpose and you are not permitted for any purpose to incorporate the contents of the Platforms or any part of it in any other work or publication (whether in hard copy, electronic or any other form) without our prior written consent. You may print or download extracts from the Platforms only for your own non-commercial use. Further, you may not copy, use, remove or alter any trademarks or logos that appear on the Platforms or on any content on the Platforms.
+
+// Links Provided:
+// The contents of the Platforms may include links to third party materials. To the maximum extent permitted by law, we will not be responsible for the contents of any linked sites or be liable for any direct or indirect loss or damage suffered by you from accessing, using, relying on or trading with third parties.
+
+// Links to our Platforms and Framing:
+// We reserve the right to prohibit links to the Platforms and you agree to remove or cease any link upon our request. You may not frame any part of the Platforms content by including advertising or other revenue generating material.
+
+// Advertising:
+// Any dealings with any advertiser or supporter appearing on the Platforms are solely between you and the advertiser or other third party. To the maximum extent permitted by law, we are not responsible or liable for any part of any such dealings or promotions.
+
+// Warranties
+// When using the Platforms, you warrant that you will:
+
+// only provide or upload information to the Platforms that is true and correct;
+// not take any action that in our opinion imposes an unreasonable load on the infrastructure of the Platforms;
+// not use the Platforms for any purpose that is unlawful or prohibited by these terms and conditions;
+// not attempt to defeat, avoid, bypass, remove, deactivate, or otherwise circumvent any software protection mechanisms in the Platforms including any mechanisms used to restrict or control the functionality of the software;
+// not remove any copyright, trade mark, or proprietary rights notices included in or on the Platforms;
+// not modify, adapt, enhance, decompile, disassemble, or reverse engineer the Platforms (or any part of the Platforms), or attempt to do the same, except as expressly permitted by the law; or
+// not attempt to disrupt or interfere with the operation of the Platforms.
+// Disclaimer:
+// We make the Platforms and its information and content available to you as a service. Use of information and content contained on the Platforms is at your own risk. We are not responsible for any adverse consequences arising out of such use. The Platforms is a general information service only. The content provided on the Platforms has not been prepared by taking into account the particular objectives, situation or needs of any individual users.
+
+// Exclusion of liability:
+// To the fullest extent permitted by law, we exclude:
+
+// all warranties and representations in relation to the Platforms (including in any content on the Platforms and services provided through the Platforms) whether express, implied, statutory or otherwise and we will not be responsible for any errors or misstatements in the Platforms.
+// all liability in relation to the Platforms whether in contract, tort (including negligence) or otherwise, for any loss or damage however caused (including direct, indirect, consequential or special loss or damage, or loss of profits, loss of data, loss of savings and loss of opportunity).
+// Indemnity:
+// To the maximum extent permitted by law, you will take all necessary action to defend and indemnify us and our officers and employees against all costs, expenses and damages incurred in connection with any claim brought by a third party against us arising from a breach by you of any of these terms and conditions.
+
+// Apple:
+// Where you download, install or use the App on an Apple mobile device, you acknowledge and agree that:
+
+// these terms and conditions are an agreement between you and us and not between you, and Apple, Inc. nor any subsidiary of Apple, Inc. (‘Apple’);
+// Apple has no responsibility or liability in respect of any matter relating to the App, including your use or possession of the App; and
+// these terms and conditions supersede any agreement between you and Apple in relation to your use of the App.
+// We and you agree that Apple, and any Apple subsidiary, are third party beneficiaries of these terms and conditions and that Apple has the right to enforce these terms and conditions against you as a third party beneficiary.
+
+// Android:
+// Where you download, install or use the App on a mobile device running an Android operating system, you acknowledge and agree that:
+
+// these terms and conditions are an agreement between you and us and not between you, and Google, Inc. nor any subsidiary of Google, Inc. (‘Google’);
+// Google has no responsibility or liability in respect of any matter relating to the App, including your use or possession of the App; and
+// these terms and conditions supersede any agreement between you and Google in relation to your use of the App.
+// We and you agree that Google, and any Google subsidiary, are third party beneficiaries of these terms and conditions and that Google has the right to enforce these terms and conditions against you as a third party beneficiary.
+
+// Change to terms and conditions:
+// We reserve the right to change these terms and conditions at any time by notifying users of the existence of the amended terms and conditions through the Platforms. By continuing to access the Platforms, you agree to be bound by the amended terms and conditions.
+
+// No Relationship:
+// These terms and conditions will not be deemed to create a partnership, joint venture, agency or employment relationship of any kind between us and you.
+
+// General:
+// If at any time we do not enforce any of these terms or conditions or grant you time or other indulgence, we will not be construed as having waived that term or condition or our rights to later enforce that or any other term or condition. Further, if any part or provision of these terms and conditions is deemed to be invalid, unenforceable or in conflict with the law, that part or provision is replaced with a provision which, as far as possible, accomplishes the original purpose of that part of provision. The remaining terms and conditions will be binding on the parties.
+
+// Law:
+// These terms and conditions are governed by and will be construed in accordance with the laws of New Zealand and you submit to the exclusive jurisdiction of the Courts of New Zealand.
+
+// Contact:
+// If you have any questions or concerns in relation to the Platforms or these terms and conditions please contact us as set out below:
+
+// Contact:	Privacy Officer
+// Phone:	+64 9 360 3691
+// Email:	privacy@myrover.co
+// Mail:	Privacy Officer
+// C/- My Rover Limited
+// PO Box 106015
+// Auckland City 1143
+// New Zealand
