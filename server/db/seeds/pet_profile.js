745672ea25ad60f7a829cf2be911022c9a12d471
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-07-22 09:40:21 +1200

    added sarah to seeds

--- server/db/seeds/pet_profile.js
+++ server/db/seeds/pet_profile.js
@@ -2,8 +2,10 @@ exports.seed = function (knex) {
   return knex('pet_profile').del()
     .then(function () {
       return knex('pet_profile').insert([
-        { id: 10000, name: 'good boy', dob: new Date(2012, 1, 1), type: 'dog', breed: 'english mastiff', gender: 'male', user_name: 'doggolover' },
-        { id: 10001, name: 'good girl', dob: new Date(2012, 1, 1), type: 'dog', breed: 'english mastiff', gender: 'female', user_name: 'doggolover' }
+        { id: 10000, name: 'Good Boy', dob: new Date(2012, 1, 1), type: 'dog', breed: 'english mastiff', gender: 'male', user_name: 'doggolover' },
+        { id: 10001, name: 'Good Girl', dob: new Date(2012, 1, 1), type: 'dog', breed: 'english mastiff', gender: 'female', user_name: 'doggolover' },
+        { id: 10002, name: 'Aurelius', dob: new Date(2012, 1, 1), type: 'otter', breed: 'sea otter', gender: 'female', user_name: 'sarahIsLive' },
+        { id: 10003, name: 'brutus', dob: new Date(2012, 1, 1), type: 'otter', breed: 'sea otter', gender: 'female', user_name: 'sarahIsLive' }
       ])
     })
 }
--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -3,9 +3,10 @@ const bcrypt = require('bcryptjs')
 exports.seed = function (knex) {
   return knex('users').del()
     .then(() => Promise.all([
-      bcrypt.hash('crackerpassword', 10)
+      bcrypt.hash('crackerpassword', 10),
+      bcrypt.hash('otterqueen', 10)
     ]))
-    .then(function (crackerHash) {
+    .then(function (crackerHash, otterqueen) {
       return knex('users').insert([
         {
           id: 1,
@@ -18,6 +19,18 @@ exports.seed = function (knex) {
           address: '42 Wallaby Way',
           city: 'Sydney',
           password: crackerHash
+        },
+        {
+          id: 2,
+          username: 'SarahIsLive',
+          phone: 221054478,
+          firstname: 'Sarah',
+          lastname: 'Emerson',
+          dob: 10011990,
+          email: 'sarahlovesotters@gmail.com',
+          address: '42 otter Way',
+          city: 'Singapore',
+          password: otterqueen
         }
       ])
     })
