a6ed498afce732a1cfdf87360714d0e9ade8148a
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-21 21:32:22 +1200

    size of log off button altered

--- client/components/SignOff.jsx
+++ client/components/SignOff.jsx
@@ -15,7 +15,7 @@ function SignOff (props) {
   }
 
   return (
-    <button className='btn cursor-pointer object-center bg-black hover:bg-gray-900 text-white font-bold rounded-md items-center justify-center col-1 h-8 w-20' onClick={handleClick}> Log Out</button>
+    <button className='btn cursor-pointer object-center bg-black hover:bg-gray-900 text-white font-bold rounded-md items-center justify-center col-1 h-10 px-2 m-2 w-20' onClick={handleClick}> Log Off</button>
   )
 }
 
