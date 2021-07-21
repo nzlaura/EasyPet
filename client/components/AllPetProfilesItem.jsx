a435becabe92f17c253d28a95fdfa23e13743dd1
adam-king-10 <adam.t.king10@gmail.com>
2021-07-22 02:03:35 +1200

    final changes to pet profile

--- client/components/AllPetProfilesItem.jsx
+++ client/components/AllPetProfilesItem.jsx
@@ -8,12 +8,11 @@ function AllPetProfilesItem (props) {
     <>
       <Link to={`/user/pets/pet/${id}`}>
         <div className='bg-gray-400 mb-20 ml-20 mt-20 rounded-lg p-4 border-black border-2'>
-          <h1 className='bg-black text-white rounded-md shadow-sm col-1 h-12 p-4 mt-2 mb-2 text-center'>{name}</h1>
+          <h1 className='bg-black text-white text-lg rounded-md shadow-sm col-1 h-12 p-4 mt-2 mb-2 text-center'>{name}</h1>
           <h2 className='bg-white rounded-md shadow-sm col-1 h-12 p-4 mt-2 mb-2'>Type: {type}</h2>
           <h2 className='bg-white rounded-md shadow-sm col-1 h-12 p-4 mt-2 mb-2'>Breed: {breed}</h2>
           <h2 className='bg-white rounded-md shadow-sm col-1 h-12 p-4 mt-2 mb-2'>Gender: {gender}</h2>
           <h2 className='bg-white rounded-md shadow-sm col-1 h-12 p-4 mt-2 mb-2'>Date of Birth: {dob}</h2>
-          <h2 className='bg-white rounded-md shadow-sm col-1 h-12 p-4 mt-2 mb-2'>ID: {id}</h2>
         </div>
       </Link>
     </>
